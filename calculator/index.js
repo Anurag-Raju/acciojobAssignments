c0a2efbe1ff9e2a2aa4fb050076170c775309402
Anurag <anuragraju.480@gmail.com>
2022-04-15 23:19:56 +0530

    Add files

---
+++ calculator/index.js
@@ -0,0 +1,9 @@
+var displayArea = document.getElementById("display");
+
+function buttonClicked(buttonText) {
+  if (displayArea.innerText === "0") {
+    displayArea.innerText = buttonText;
+  } else {
+    displayArea.innerText = displayArea.innerText + buttonText;
+  }
+}
