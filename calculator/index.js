f45eaa50a55b07a785439b1ac24da008e1dd1c4a
Anurag <anuragraju.480@gmail.com>
2022-04-16 10:31:39 +0530

    Added events

--- calculator/index.js
+++ calculator/index.js
@@ -1,9 +1,35 @@
 var displayArea = document.getElementById("display");
 
+function isOperator(buttonText) {
+  if (
+    buttonText === "+" ||
+    buttonText === "-" ||
+    buttonText === "*" ||
+    buttonText === "/" ||
+    buttonText === "%"
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function buttonClicked(buttonText) {
   if (displayArea.innerText === "0") {
-    displayArea.innerText = buttonText;
+    if (buttonText != "00") {
+      if (buttonText == "." || isOperator(buttonText)) {
+        displayArea.innerText = displayArea.innerText + buttonText;
+      } else {
+        displayArea.innerText = buttonText;
+      }
+    }
   } else {
-    displayArea.innerText = displayArea.innerText + buttonText;
+    if (
+      isOperator(displayArea.innerText[displayArea.innerText.length - 1]) &&
+      isOperator(buttonText)
+    ) {
+    } else {
+      displayArea.innerText = displayArea.innerText + buttonText;
+    }
   }
 }
